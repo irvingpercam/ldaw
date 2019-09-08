236128df44b3fc270f47cae1e273e661152e207c
Irving Alain Aguilar Pérez <a01703171@itesm.mx>
2019-09-08 13:18:17 -0500

    Update: Changes on src/index.js Board handClick

--- tutorial_react/my-app/src/index.js
+++ tutorial_react/my-app/src/index.js
@@ -4,23 +4,37 @@ import './index.css';
 class Square extends React.Component {
   /*Constructor para estado
   Queremos que el componente Square "recuerde" los clicks, a su vez llenando la casilla
-  correspondiente con una "X". The current value of square is stored in "this.state" and
-  it is changed when the Square is clicked.
+  correspondiente con una "X". El valor actual de square es guardado en "this.state" y
+  este cambia cuando Square es clickeado.
   */
-  constructor(props){
-    /**
-     * En las clases de JavaScript siempre se debe llamar al método super cuando se define
-     * un constructor de una subclase.
-     * 
-     * Todas clases de componentes de React que tienen un constructor deben empezar con una
-     * llamada a super (props).
-     */
-    super(props);
-    this.state = {
-      value: null,
-    };
-  }
+  // constructor(props){
+  //   /**
+  //    * En las clases de JavaScript siempre se debe llamar al método super cuando se define
+  //    * un constructor de una subclase.
+  //    * 
+  //    * Todas clases de componentes de React que tienen un constructor deben empezar con una
+  //    * llamada a super (props).
+  //    */
+  //   super(props);
+  //   this.state = {
+  //     value: null,
+  //   };
+  // }
+  // Este constructor queda como comentario, ya que en cierta parte del tutorial, el constructor
+  // se debe eliminar porque el componente ya no hace seguimiento del estado del juego.
   
+  /**
+    * Cuando un cuadrado es clickeado, la función onClick provista por el componente Board
+    * es llamada.
+    * 
+    * 1. El prop onClick le indica a React que establezca un escuchador del evento Click.
+    * 2. Cuando el botón es clickeado, React llama al manejador de evento onClick que está
+    * definido en el método render() de Square.
+    * 3. El manejador de evento, llama a this.props.onClick(). El prop onClick del compone-
+    * nte onClick está especificado por el componente Board.
+    * 4. Dado que en Board pasa onClick = {() => this.handleClick{i}} a Square, el compone-
+    * nte Square llama a this.handleClick(i) cuando es clickeado.
+    */
     render() {
       return (
         <button 
@@ -33,17 +47,56 @@ class Square extends React.Component {
            * a setState en un componente, React actualiza automáticamente los componentes hijos
            * dentro del mismo.
            */
-          onClick={() => this.setState({value: 'X'})}
+
+          onClick = {() => this.props.onClick()}
           >
-          {this.state.value}
+          {this.props.value}
         </button>
       );
     }
   }
-  
+/**
+ * Se va a almacenar el estado del juego en el componente padre Board en vez de cada componente
+ * Square. El componente Board puede decirle a cada cuadrado que mostrar pasándole un prop.
+ * 
+ * Para tener recopilar datos de múltiples hijos, o comunicar dos componentes hijos entre sí,
+ * necesitamos declarar el estado compartido de su componente padre. El componente padre puede
+ * pasar el estado hacia los hijos usando props, así manteniendo los componentes hijos sincro-
+ * nizados entre ellos y con su componente padre.
+ */  
 class Board extends React.Component {
-renderSquare(i) {
-    return <Square value = {i} />;
+/**
+ * Se añade un constructor para establecer el estado inicial de Board para contener un arreglo
+ * con 9 valores nulos (null). Estos 9 valores corresponden a los 9 cuadrados.
+ */
+  constructor(props){
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+    };
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares: squares});
+  }
+
+  /**
+   * Modificamos este método renderSquare para que lea desde el arreglo.
+   * 
+   * Pasamos una función como prop desde Board a Square y hacemos que Square llame a esa función
+   * cuando un cuadrado sea clickeado.
+   * 
+   * Estamos pasando dos props desde Board a Square: value y onClick.
+   */
+  renderSquare(i) {
+    return (
+      <Square 
+        value = {this.state.squares[i]}
+        onClick = {() => this.handleClick(i)} 
+        />
+    );
 }
 
 render() {
